a737f5e42896d4650530069bb82e19e2c3fa3ace
JSaucourt <jysru@pm.me>
2023-03-13 15:08:51 +0100

    Reformatting

--- pim_tmr.py
+++ pim_tmr.py
@@ -11,29 +11,29 @@ def mse_cols(x, y):
     return np.mean(np.square(x - y), axis=0)
 
 def random_init(n, m):
-    return np.random.rand(m,n)*np.exp(1j*2*np.pi*np.random.rand(m,n))
+    return np.random.rand(m,n) * np.exp(1j * 2 * np.pi * np.random.rand(m,n))
 
 def wirtinger_initialization(A, B):
     n, m, N = A.shape[1], B.shape[1], B.shape[0]
-    X_init = np.zeros(shape=(n,m), dtype=np.complex64)
+    X_init = np.zeros(shape=(n, m), dtype=np.complex64)
     B = np.square(B)
 
     for k in range(m):
         lbda = 0
         for j in range(N):
-            lbda += np.square(np.linalg.norm(A[j,:]))
-        lbda = np.sqrt(n * np.sum(B[:,k]) / lbda)
-        _, v = np.linalg.eig(np.dot(np.transpose(np.conj(A)), np.dot(np.diag(B[:,k]), A)) / N)
-        X_init[:,k] = lbda * v[:,0]
+            lbda += np.square(np.linalg.norm(A[j, :]))
+        lbda = np.sqrt(n * np.sum(B[:, k]) / lbda)
+        _, v = np.linalg.eig(np.dot(np.transpose(np.conj(A)), np.dot(np.diag(B[:, k]), A)) / N)
+        X_init[:, k] = lbda * v[:, 0]
 
     return X_init
 
 
-def pim_tmr(A, B, max_iter=10000, tol=1e-3, tol_stag=1e-3, max_stag=10, init_wirtinger: bool = False):
+def pim_tmr(A, B, max_iter: int = 10000, tol: float = 1e-3, tol_stag: float = 1e-3, max_stag: int = 10, init_wirtinger: bool = False):
     n, m = A.shape[1], B.shape[1]
     U, s, Vh = np.linalg.svd(A, full_matrices=False)
     S = np.diag(s)
-    Bnorm = B/np.linalg.norm(B, ord='fro')
+    Bnorm = B / np.linalg.norm(B, ord='fro')
 
     if init_wirtinger:
         Xk = wirtinger_initialization(A, B)
@@ -52,8 +52,8 @@ def pim_tmr(A, B, max_iter=10000, tol=1e-3, tol_stag=1e-3, max_stag=10, init_wir
         Xk[:, active_cols] = np.dot(Vh.conj().T, np.linalg.solve(S, tmp) )
 
         Bk = np.abs(np.dot(A, Xk))
-        betak = mse_cols(Bk/np.linalg.norm(Bk, ord='fro'), Bnorm)
-        MSE = mse(Bk/np.linalg.norm(Bk, ord='fro'), Bnorm)
+        betak = mse_cols(Bk / np.linalg.norm(Bk, ord='fro'), Bnorm)
+        MSE = mse(Bk / np.linalg.norm(Bk, ord='fro'), Bnorm)
 
         convergence_check = (betak < tol)
         converged_cols[active_cols] = convergence_check[active_cols] 
@@ -62,7 +62,7 @@ def pim_tmr(A, B, max_iter=10000, tol=1e-3, tol_stag=1e-3, max_stag=10, init_wir
         normresanc = normres
         normres = betak
 
-        stag_cols[active_cols] = (np.abs(normres[active_cols] - normresanc[active_cols])/normres[active_cols] < tol_stag)
+        stag_cols[active_cols] = (np.abs(normres[active_cols] - normresanc[active_cols]) / normres[active_cols] < tol_stag)
         i_stags[stag_cols] = i_stags[stag_cols] + 1
 
         cols_reset = (i_stags >= max_stag)
@@ -94,9 +94,9 @@ def compare_matrices(X1, X2):
 
     fig, axs = plt.subplots(1, 2)
     pl0 = axs[0].imshow(np.abs(X1.T) - np.abs(X2.T))
-    diff_angle = np.angle(np.exp(1j*(np.angle(X1.T) - np.angle(X2.T))))
-    diff_means = np.mean(diff_angle, axis=1).reshape((diff_angle.shape[0],1))
-    diff_angle = np.angle(np.exp(1j*(diff_angle - diff_means)))
+    diff_angle = np.angle(np.exp(1j * (np.angle(X1.T) - np.angle(X2.T))))
+    diff_means = np.mean(diff_angle, axis=1).reshape((diff_angle.shape[0], 1))
+    diff_angle = np.angle(np.exp(1j * (diff_angle - diff_means)))
     pl1 = axs[1].imshow(diff_angle, cmap="twilight")
 
     plt.colorbar(pl0, ax=axs[0])
@@ -108,13 +108,11 @@ def compare_matrices(X1, X2):
 if __name__ == "__main__":
     N = 2000
     n = 32
-    # m = 288*n
     m = 12*n
 
-    A = np.random.rand(N,n)*np.exp(1j*2*np.pi*np.random.rand(N,n))
-    X = np.random.rand(n,m)*np.exp(1j*2*np.pi*np.random.rand(n,m))
+    A = np.random.rand(N,n) * np.exp(1j * 2 * np.pi * np.random.rand(N,n))
+    X = np.random.rand(n,m) * np.exp(1j * 2 * np.pi * np.random.rand(n,m))
     B = np.abs(np.dot(A, X))
-    # B = np.abs(B + 0.1*np.random.randn(B.shape[0], B.shape[1]))
 
     X_est = pim_tmr(A, B, max_iter=1000, tol=8e-9, max_stag=20)
     B_est = np.abs(np.dot(A, X_est))
