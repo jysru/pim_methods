511127e406d2d66c413fa72b8e32b0c1b0abb6a7
JSaucourt <jysru@pm.me>
2023-03-16 14:45:18 +0100

    Added global criterion for columns reset

--- pim_tmr.py
+++ pim_tmr.py
@@ -29,16 +29,19 @@ def wirtinger_initialization(A, B):
     return X_init
 
 
-def pim_tmr(A, B, max_iter: int = 10000, tol: float = 1e-3, tol_stag: float = 1e-3, max_stag: int = 10, init_wirtinger: bool = False, disable_outputs: bool = False):
+def pim_tmr(A, B, max_iter: int = 10000, tol: float = 1e-3, tol_stag: float = 1e-3, max_stag: int = 10, init_matrix = None, init_wirtinger: bool = False, disable_outputs: bool = False, col_per_col: bool = True):
     n, m = A.shape[1], B.shape[1]
     U, s, Vh = np.linalg.svd(A, full_matrices=False)
     S = np.diag(s)
     Bnorm = B / np.linalg.norm(B, ord='fro')
 
-    if init_wirtinger:
-        Xk = wirtinger_initialization(A, B)
+    if init_matrix is not None:
+        Xk = init_matrix
     else:
-        Xk = random_init(m, n)
+        if init_wirtinger:
+            Xk = wirtinger_initialization(A, B)
+        else:
+            Xk = random_init(m, n)
     active_cols = np.ones(shape=(m), dtype=bool)
     converged_cols = np.logical_not(active_cols)
     stag_cols = np.zeros(shape=(m), dtype=bool)
@@ -46,6 +49,7 @@ def pim_tmr(A, B, max_iter: int = 10000, tol: float = 1e-3, tol_stag: float = 1e
     
     normres = np.zeros(shape=(m), dtype=float)
     restart = 0
+    i_stag = 0
     for iter in range(max_iter):
         tmp = B[:, active_cols] * np.exp(1j * np.angle(np.dot(A, Xk[:, active_cols])))
         tmp = np.dot(U.conj().T, tmp) 
@@ -62,17 +66,26 @@ def pim_tmr(A, B, max_iter: int = 10000, tol: float = 1e-3, tol_stag: float = 1e
         normresanc = normres
         normres = betak
 
-        stag_cols[active_cols] = (np.abs(normres[active_cols] - normresanc[active_cols]) / normres[active_cols] < tol_stag)
-        i_stags[stag_cols] = i_stags[stag_cols] + 1
+        if col_per_col:
+            stag_cols[active_cols] = (np.abs(normres[active_cols] - normresanc[active_cols]) / normres[active_cols] < tol_stag)
+            i_stags[stag_cols] = i_stags[stag_cols] + 1
 
-        cols_reset = (i_stags >= max_stag)
-        if np.any(cols_reset):
-            Xnew = random_init(np.sum(cols_reset), n)
-            Xk[:, cols_reset] = Xnew
-            if not disable_outputs:
-                print(f"Restarting {np.sum(cols_reset)} columns ")
-            i_stags[cols_reset] = 0
-            restart += 1
+            cols_reset = (i_stags >= max_stag)
+            if np.any(cols_reset):
+                Xnew = random_init(np.sum(cols_reset), n)
+                Xk[:, cols_reset] = Xnew
+                if not disable_outputs:
+                    print(f"Restarting {np.sum(cols_reset)} columns ")
+                i_stags[cols_reset] = 0
+                restart += 1
+        else:
+            if np.any((np.abs(normres[active_cols] - normresanc[active_cols]) / normres[active_cols] < tol_stag)):
+                i_stag += 1
+            if i_stag > max_stag:
+                Xnew = random_init(np.sum(active_cols), n)
+                Xk[:, active_cols] = Xnew
+                i_stag = 0
+                restart += 1
 
         if not disable_outputs:
             print(f"i={iter:5.0f}  mse_glob={MSE:1.3e}  mse_convs={np.mean(betak[converged_cols]):1.3e}  mse_acts={np.mean(betak[active_cols]):1.3e}  act={np.sum(active_cols):5.0f}  convs={np.sum(converged_cols):5.0f}")
