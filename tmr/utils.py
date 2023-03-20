3b1bfe844249d7f3750d4aef54025b752c13ef3f
JSaucourt <jysru@pm.me>
2023-03-20 16:38:55 +0100

    Added select_pixels and crop_img

--- tmr/utils.py
+++ tmr/utils.py
@@ -10,3 +10,14 @@ def get_subsets(A, B, N: int, randomize: bool = False):
     else:
         return (A[:N, :], B[:N, :])
 
+
+def select_pixels(img, idxs):
+    idxs = idxs.astype(int)
+    X, Y = np.meshgrid(idxs, idxs)
+    img = img[X.flatten(), Y.flatten()]
+    img = np.reshape(img, (len(idxs), len(idxs)))
+    return img
+
+
+def crop_img(img, crop: int = 3):
+    return img[crop:-crop, crop:-crop]
